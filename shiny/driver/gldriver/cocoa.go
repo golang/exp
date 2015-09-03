41bb8dc7b4ad0550b2089a9ef256163119b7323d
Nigel Tao <nigeltao@golang.org>
2015-09-03 12:41:59 +1000

    shiny/driver/gldriver: fold cocoa.h into cocoa.go.
    The .h file is only included in one place. We might as well inline it.
    
    Change-Id: I69726ffb89c45a8a9b1b3db1e462fa3021121cd5
    Reviewed-on: https://go-review.googlesource.com/14230
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- shiny/driver/gldriver/cocoa.go
+++ shiny/driver/gldriver/cocoa.go
@@ -14,7 +14,15 @@ package gldriver
 #import <Carbon/Carbon.h> // for HIToolbox/Events.h
 #import <Cocoa/Cocoa.h>
 #include <pthread.h>
-#include "cocoa.h"
+#include <stdint.h>
+
+void startDriver();
+void stopDriver();
+void makeCurrentContext(uintptr_t ctx);
+uintptr_t doNewWindow(int width, int height);
+uintptr_t doShowWindow(uintptr_t id);
+void doCloseWindow(uintptr_t id);
+uint64_t threadID();
 */
 import "C"
 
--- shiny/driver/gldriver/cocoa.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-void startDriver();
-void stopDriver();
-void makeCurrentContext(uintptr_t ctx);
-uintptr_t doNewWindow(int width, int height);
-uintptr_t doShowWindow(uintptr_t id);
-void doCloseWindow(uintptr_t id);
-uint64_t threadID();
