4169c3ff092dc05769e9c88c5678b34667db3daf
Jeremy Jackins <jeremyjackins@gmail.com>
2015-12-16 17:27:32 +0900

    shiny/driver/gldriver: keyboard events for cocoa
    Change-Id: I320d8475bfbf163c5b1e472af01500010bc7303a
    Reviewed-on: https://go-review.googlesource.com/17881
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- shiny/driver/gldriver/cocoa.m
+++ shiny/driver/gldriver/cocoa.m
@@ -145,7 +145,52 @@ uint64 threadID() {
 @end
 
 @implementation WindowResponder
-// TODO(crawshaw): key events
+{
+	uintptr_t windowID;
+}
+
+- (id)initWithWindowID:(uintptr_t)id {
+    windowID = id;
+    return self;
+}
+
+- (void)keyDown:(NSEvent *)theEvent {
+	[self key:theEvent];
+}
+- (void)keyUp:(NSEvent *)theEvent {
+	[self key:theEvent];
+}
+- (void)key:(NSEvent *)theEvent {
+	NSRange range = [theEvent.characters rangeOfComposedCharacterSequenceAtIndex:0];
+
+	uint8_t buf[4] = {0, 0, 0, 0};
+	if (![theEvent.characters getBytes:buf
+			maxLength:4
+			usedLength:nil
+			encoding:NSUTF32LittleEndianStringEncoding
+			options:NSStringEncodingConversionAllowLossy
+			range:range
+			remainingRange:nil]) {
+		NSLog(@"failed to read key event %@", theEvent);
+		return;
+	}
+
+	uint32_t rune = (uint32_t)buf[0]<<0 | (uint32_t)buf[1]<<8 | (uint32_t)buf[2]<<16 | (uint32_t)buf[3]<<24;
+
+	uint8_t direction;
+	if ([theEvent isARepeat]) {
+		direction = 0;
+	} else if (theEvent.type == NSKeyDown) {
+		direction = 1;
+	} else {
+		direction = 2;
+	}
+	keyEvent((GoUintptr)windowID, (int32_t)rune, direction, theEvent.keyCode, theEvent.modifierFlags);
+}
+
+- (void)flagsChanged:(NSEvent *)theEvent {
+	flagEvent((GoUintptr)windowID, theEvent.modifierFlags);
+}
 @end
 
 @interface AppDelegate : NSObject<NSApplicationDelegate>
@@ -220,7 +265,7 @@ uintptr_t doNewWindow(int width, int height) {
 		[window setContentView:view];
 		[window setDelegate:view];
 
-		window.nextResponder = [[WindowResponder alloc] init];
+		window.nextResponder = [[WindowResponder alloc] initWithWindowID:(uintptr_t)view];
 	});
 
 	return (uintptr_t)view;
