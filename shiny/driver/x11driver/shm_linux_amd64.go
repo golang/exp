9f12c88e90196c3087e94bee981d7c0c57f3b468
Nigel Tao <nigeltao@golang.org>
2016-02-04 18:07:00 +1100

    shiny/driver/x11driver: add more context to shm error messages.
    Change-Id: Ie9ab42dcfce200e202ac4e11dfa24b743f12e649
    Reviewed-on: https://go-review.googlesource.com/19215
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- shiny/driver/x11driver/shm_linux_amd64.go
+++ shiny/driver/x11driver/shm_linux_amd64.go
@@ -5,6 +5,7 @@
 package x11driver
 
 import (
+	"fmt"
 	"syscall"
 	"unsafe"
 )
@@ -19,15 +20,15 @@ const (
 func shmOpen(size int) (shmid uintptr, addr unsafe.Pointer, err error) {
 	shmid, _, errno0 := syscall.RawSyscall(syscall.SYS_SHMGET, ipcPrivate, uintptr(size), ipcCreat|0600)
 	if errno0 != 0 {
-		return 0, unsafe.Pointer(uintptr(0)), errno0
+		return 0, unsafe.Pointer(uintptr(0)), fmt.Errorf("shmget: %v", errno0)
 	}
 	p, _, errno1 := syscall.RawSyscall(syscall.SYS_SHMAT, shmid, 0, 0)
 	_, _, errno2 := syscall.RawSyscall(syscall.SYS_SHMCTL, shmid, ipcRmID, 0)
 	if errno1 != 0 {
-		return 0, unsafe.Pointer(uintptr(0)), errno1
+		return 0, unsafe.Pointer(uintptr(0)), fmt.Errorf("shmat: %v", errno1)
 	}
 	if errno2 != 0 {
-		return 0, unsafe.Pointer(uintptr(0)), errno2
+		return 0, unsafe.Pointer(uintptr(0)), fmt.Errorf("shmctl: %v", errno2)
 	}
 	return shmid, unsafe.Pointer(p), nil
 }
@@ -35,7 +36,7 @@ func shmOpen(size int) (shmid uintptr, addr unsafe.Pointer, err error) {
 func shmClose(p unsafe.Pointer) error {
 	_, _, errno := syscall.RawSyscall(syscall.SYS_SHMDT, uintptr(p), 0, 0)
 	if errno != 0 {
-		return errno
+		return fmt.Errorf("shmdt: %v", errno)
 	}
 	return nil
 }
