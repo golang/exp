d9f7a4f94a755fba006dbf8df2fa94ca5c18a938
Alex Brainman <alex.brainman@gmail.com>
2016-02-28 13:13:37 +1100

    shiny/driver/windriver: sort //sys lines in syscall_windows.go
    Change-Id: I5f00c02b108b3a5f2bacbcf59bd7fd223762e247
    Reviewed-on: https://go-review.googlesource.com/19948
    Reviewed-by: Nigel Tao <nigeltao@golang.org>

--- shiny/driver/windriver/zsyscall_windows.go
+++ shiny/driver/windriver/zsyscall_windows.go
@@ -9,72 +9,21 @@ import "golang.org/x/exp/shiny/driver/internal/win32"
 var _ unsafe.Pointer
 
 var (
-	modgdi32   = syscall.NewLazyDLL("gdi32.dll")
 	modmsimg32 = syscall.NewLazyDLL("msimg32.dll")
+	modgdi32   = syscall.NewLazyDLL("gdi32.dll")
 	moduser32  = syscall.NewLazyDLL("user32.dll")
 
-	procCreateDIBSection       = modgdi32.NewProc("CreateDIBSection")
-	procCreateCompatibleDC     = modgdi32.NewProc("CreateCompatibleDC")
-	procDeleteDC               = modgdi32.NewProc("DeleteDC")
-	procSelectObject           = modgdi32.NewProc("SelectObject")
 	procAlphaBlend             = modmsimg32.NewProc("AlphaBlend")
 	procBitBlt                 = modgdi32.NewProc("BitBlt")
-	procCreateSolidBrush       = modgdi32.NewProc("CreateSolidBrush")
-	procFillRect               = moduser32.NewProc("FillRect")
-	procDeleteObject           = modgdi32.NewProc("DeleteObject")
 	procCreateCompatibleBitmap = modgdi32.NewProc("CreateCompatibleBitmap")
+	procCreateCompatibleDC     = modgdi32.NewProc("CreateCompatibleDC")
+	procCreateDIBSection       = modgdi32.NewProc("CreateDIBSection")
+	procCreateSolidBrush       = modgdi32.NewProc("CreateSolidBrush")
+	procDeleteDC               = modgdi32.NewProc("DeleteDC")
+	procDeleteObject           = modgdi32.NewProc("DeleteObject")
+	procFillRect               = moduser32.NewProc("FillRect")
+	procSelectObject           = modgdi32.NewProc("SelectObject")
 )
-
-func _CreateDIBSection(dc syscall.Handle, bmi *_BITMAPINFO, usage uint32, bits **byte, section syscall.Handle, offset uint32) (bitmap syscall.Handle, err error) {
-	r0, _, e1 := syscall.Syscall6(procCreateDIBSection.Addr(), 6, uintptr(dc), uintptr(unsafe.Pointer(bmi)), uintptr(usage), uintptr(unsafe.Pointer(bits)), uintptr(section), uintptr(offset))
-	bitmap = syscall.Handle(r0)
-	if bitmap == 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
-
-func _CreateCompatibleDC(dc win32.HDC) (newdc win32.HDC, err error) {
-	r0, _, e1 := syscall.Syscall(procCreateCompatibleDC.Addr(), 1, uintptr(dc), 0, 0)
-	newdc = win32.HDC(r0)
-	if newdc == 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
-
-func _DeleteDC(dc win32.HDC) (err error) {
-	r1, _, e1 := syscall.Syscall(procDeleteDC.Addr(), 1, uintptr(dc), 0, 0)
-	if r1 == 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
-
-func _SelectObject(dc win32.HDC, gdiobj syscall.Handle) (newobj syscall.Handle, err error) {
-	r0, _, e1 := syscall.Syscall(procSelectObject.Addr(), 2, uintptr(dc), uintptr(gdiobj), 0)
-	newobj = syscall.Handle(r0)
-	if newobj == 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
 
 func _AlphaBlend(dcdest win32.HDC, xoriginDest int32, yoriginDest int32, wDest int32, hDest int32, dcsrc win32.HDC, xoriginSrc int32, yoriginSrc int32, wsrc int32, hsrc int32, ftn uintptr) (err error) {
 	r1, _, e1 := syscall.Syscall12(procAlphaBlend.Addr(), 11, uintptr(dcdest), uintptr(xoriginDest), uintptr(yoriginDest), uintptr(wDest), uintptr(hDest), uintptr(dcsrc), uintptr(xoriginSrc), uintptr(yoriginSrc), uintptr(wsrc), uintptr(hsrc), uintptr(ftn), 0)
@@ -100,6 +49,45 @@ func _BitBlt(dcdest win32.HDC, xdest int32, ydest int32, width int32, height int
 	return
 }
 
+func _CreateCompatibleBitmap(dc win32.HDC, width int32, height int32) (bitmap syscall.Handle, err error) {
+	r0, _, e1 := syscall.Syscall(procCreateCompatibleBitmap.Addr(), 3, uintptr(dc), uintptr(width), uintptr(height))
+	bitmap = syscall.Handle(r0)
+	if bitmap == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func _CreateCompatibleDC(dc win32.HDC) (newdc win32.HDC, err error) {
+	r0, _, e1 := syscall.Syscall(procCreateCompatibleDC.Addr(), 1, uintptr(dc), 0, 0)
+	newdc = win32.HDC(r0)
+	if newdc == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func _CreateDIBSection(dc syscall.Handle, bmi *_BITMAPINFO, usage uint32, bits **byte, section syscall.Handle, offset uint32) (bitmap syscall.Handle, err error) {
+	r0, _, e1 := syscall.Syscall6(procCreateDIBSection.Addr(), 6, uintptr(dc), uintptr(unsafe.Pointer(bmi)), uintptr(usage), uintptr(unsafe.Pointer(bits)), uintptr(section), uintptr(offset))
+	bitmap = syscall.Handle(r0)
+	if bitmap == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
 func _CreateSolidBrush(color _COLORREF) (brush syscall.Handle, err error) {
 	r0, _, e1 := syscall.Syscall(procCreateSolidBrush.Addr(), 1, uintptr(color), 0, 0)
 	brush = syscall.Handle(r0)
@@ -113,8 +101,8 @@ func _CreateSolidBrush(color _COLORREF) (brush syscall.Handle, err error) {
 	return
 }
 
-func _FillRect(dc win32.HDC, rc *_RECT, brush syscall.Handle) (err error) {
-	r1, _, e1 := syscall.Syscall(procFillRect.Addr(), 3, uintptr(dc), uintptr(unsafe.Pointer(rc)), uintptr(brush))
+func _DeleteDC(dc win32.HDC) (err error) {
+	r1, _, e1 := syscall.Syscall(procDeleteDC.Addr(), 1, uintptr(dc), 0, 0)
 	if r1 == 0 {
 		if e1 != 0 {
 			err = error(e1)
@@ -137,10 +125,22 @@ func _DeleteObject(object syscall.Handle) (err error) {
 	return
 }
 
-func _CreateCompatibleBitmap(dc win32.HDC, width int32, height int32) (bitmap syscall.Handle, err error) {
-	r0, _, e1 := syscall.Syscall(procCreateCompatibleBitmap.Addr(), 3, uintptr(dc), uintptr(width), uintptr(height))
-	bitmap = syscall.Handle(r0)
-	if bitmap == 0 {
+func _FillRect(dc win32.HDC, rc *_RECT, brush syscall.Handle) (err error) {
+	r1, _, e1 := syscall.Syscall(procFillRect.Addr(), 3, uintptr(dc), uintptr(unsafe.Pointer(rc)), uintptr(brush))
+	if r1 == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func _SelectObject(dc win32.HDC, gdiobj syscall.Handle) (newobj syscall.Handle, err error) {
+	r0, _, e1 := syscall.Syscall(procSelectObject.Addr(), 2, uintptr(dc), uintptr(gdiobj), 0)
+	newobj = syscall.Handle(r0)
+	if newobj == 0 {
 		if e1 != 0 {
 			err = error(e1)
 		} else {
