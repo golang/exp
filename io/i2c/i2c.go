b3108acf52c899c442147b56ec21a9cd1ea51d76
Jaana Burcu Dogan <jbd@google.com>
2016-04-06 14:31:41 -0700

    io/i2c: use devfs if no other Opener is specified
    Change-Id: I51712858f77f94d51b0334dd1cd28c4d8e6ac089
    Reviewed-on: https://go-review.googlesource.com/21621
    Reviewed-by: Jaana Burcu Dogan <jbd@google.com>

--- io/i2c/i2c.go
+++ io/i2c/i2c.go
@@ -37,6 +37,9 @@ func (d *Device) Close() error {
 
 // Open opens an I2C device with the given I2C address on the specified bus.
 func Open(o driver.Opener, bus, addr int) (*Device, error) {
+	if o == nil {
+		o = &DevFS{}
+	}
 	conn, err := o.Open(bus, addr)
 	if err != nil {
 		return nil, err
