452ccb56db81f3462a30c45640919ad3b7bd53ba
Jaana Burcu Dogan <jbd@google.com>
2016-05-21 14:30:50 -0700

    io: build devfs drivers only on linux
    Updates golang/go#11811.
    
    Change-Id: Iecf3eead2fb1c619a2409bfc3e59ef49578ebc81
    Reviewed-on: https://go-review.googlesource.com/23318
    Reviewed-by: Minux Ma <minux@golang.org>

--- io/i2c/example/displayip/main.go
+++ io/i2c/example/displayip/main.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux
+
 // Package main contains a program that displays the IPv4 address
 // of the machine on an a Grove-LCD RGB backlight.
 package main
