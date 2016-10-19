585538dc760125820761264073c34017ffc44fb8
Nigel Tao <nigeltao@golang.org>
2016-10-20 10:21:12 +1100

    shiny/iconvg: add note to think about Right-To-Left languages.
    Change-Id: I583c0162e24cbd84170b48cb463e70ddfe64823b
    Reviewed-on: https://go-review.googlesource.com/31531
    Reviewed-by: David Crawshaw <crawshaw@golang.org>

--- shiny/iconvg/doc.go
+++ shiny/iconvg/doc.go
@@ -552,4 +552,10 @@ package iconvg
 // TODO: shapes (circles, rects) and strokes? Or can we assume that authoring
 // tools will convert shapes and strokes to paths?
 
+// TODO: mark somehow that a graphic (such as a back arrow) should be flipped
+// horizontally or its paths otherwise varied when presented in a Right-To-Left
+// context, such as among Arabic and Hebrew text? Or should that be the
+// responsibility of higher layers, selecting different IconVG graphics based
+// on context, the way they would select different PNG graphics.
+
 // TODO: hinting?
