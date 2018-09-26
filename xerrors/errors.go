344881521eb66a48a03a5b55b4be147ab3f462e8
Damien Neil <dneil@google.com>
2018-09-26 11:45:18 -0700

    xerrors: new package
    This package will implement a version of the Go 2 draft designs for
    error inspection and printing.
    
    The initial commit is just a copy of the standard library errors
    package.
    
    Change-Id: I411e768260238ff877964fb01445508ea992f164
    Reviewed-on: https://go-review.googlesource.com/c/137795
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Marcel van Lohuizen <mpvl@golang.org>

---
+++ xerrors/errors.go
@@ -0,0 +1,25 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package xerrors implements functions to manipulate errors.
+//
+// This package implements the Go 2 draft designs for error inspection and printing:
+//   https://go.googlesource.com/proposal/+/master/design/go2draft.md
+//
+// This is an EXPERIMENTAL package, and may change in arbitrary ways without notice.
+package xerrors
+
+// errorString is a trivial implementation of error.
+type errorString struct {
+	s string
+}
+
+// New returns an error that formats as the given text.
+func New(text string) error {
+	return &errorString{text}
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
