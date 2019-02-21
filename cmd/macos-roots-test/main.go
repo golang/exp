438050ddec5e7f808979ed57d041cebbc8e2d8a9
Brad Fitzpatrick <bradfitz@golang.org>
2019-02-21 21:14:45 +0000

    cmd/macos-roots-test, ebnflint: fix tests so trybots work
    Trybots used to work on the exp repo. Fix this to restore trybots
    passing.
    
    Change-Id: I06240de6cb1c526236c7a0e0a6c7f0d44498c1a3
    Reviewed-on: https://go-review.googlesource.com/c/163302
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

--- cmd/macos-roots-test/main.go
+++ cmd/macos-roots-test/main.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin
+
 // Command macOS-roots-test runs crypto/x509.TestSystemRoots as a
 // stand-alone binary for crowdsourced testing.
 package main
