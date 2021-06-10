f956177b079c1c2e99c7aa087a11d1f7889332a2
Jonathan Amsterdam <jba@google.com>
2021-06-10 11:31:29 -0400

    jsonrpc2: JSON-RPC 2.0 implementation
    See https://www.jsonrpc.org/specification.
    
    Forked from golang.org/x/tool/internal/jsonrpc2_v2 at
    commit 13cf844527f48de3074270bb538dff67117a6655.
    
    Changed to use golang.org/x/exp/event.
    
    This CL also contains a fork of golang.org/x/tools/internal/stack/...
    at the same commit, needed for testing.
    
    Change-Id: Ifc634c2b8e368a51fc43a7c0fab46cfa0ed5464b
    Reviewed-on: https://go-review.googlesource.com/c/exp/+/326676
    Trust: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>

---
+++ jsonrpc2/defs.go
@@ -0,0 +1,31 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package jsonrpc2
+
+import (
+	"golang.org/x/exp/event"
+	"golang.org/x/exp/event/keys"
+)
+
+var (
+	Method       = keys.String("method")
+	RPCID        = keys.String("id")
+	RPCDirection = keys.String("direction")
+	Error        = keys.Value("error")
+	StatusCode   = keys.String("status.code")
+)
+
+var (
+	Started       = event.NewCounter("started", "Count of started RPCs.")
+	Finished      = event.NewCounter("finished", "Count of finished RPCs (includes error).")
+	ReceivedBytes = event.NewIntDistribution("received_bytes", "Bytes received.") //, unit.Bytes)
+	SentBytes     = event.NewIntDistribution("sent_bytes", "Bytes sent.")         //, unit.Bytes)
+	Latency       = event.NewDuration("latency", "Elapsed time of an RPC.")       //, unit.Milliseconds)
+)
+
+const (
+	Inbound  = "in"
+	Outbound = "out"
+)
