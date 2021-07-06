41231fe85c9304db4ccd83075221b2a50f6c5dfd
Nigel Tao <nigeltao@golang.org>
2021-07-06 23:42:51 +1000

    shiny/iconvg: add UpgradeToFileFormatVersion1
    The iconvg Go implementation started at golang.org/x/exp/shiny/iconvg in
    2016, speaking a file format retroactively named FFV0 (File Format
    Version 0). The github.com/google/iconvg/src/go/* packages were created
    more recently in 2021 and will speak a revised format, FFV1.
    
    The long term plan is for the github.com/... packages to *only* speak
    FFV1. The golang.org/... package (this package) will continue to speak
    FFV0 directly and will additionally delegate decoding FFV1 to the
    github.com/... packages. FFV0 will be considered a deprecated experiment
    (it was marked EXPERIMENTAL ever since its inception). See
    https://github.com/google/iconvg/issues/4
    
    This commit, which decodes FFV0 and encodes FFV1, lives in the
    golang.org/... packages, since it involves FFV0. Testing it (beyond the
    basic consistency checks in upgrade_test.go) lives in the github.com/...
    packages, since that involves decoding FFV1. For example, some outputs
    of the UpgradeToFileFormatVersion1 function (added by this commit) were
    checked in as https://github.com/google/iconvg/commit/c98b08c
    
    Change-Id: Ib5861ae97928cf31faf207b915568532e2624f09
    Reviewed-on: https://go-review.googlesource.com/c/exp/+/332989
    Trust: Nigel Tao <nigeltao@golang.org>
    Reviewed-by: Andrew Gerrand <adg@golang.org>

---
+++ shiny/iconvg/upgrade_test.go
@@ -0,0 +1,47 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package iconvg
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestUpgradeToFileFormatVersion1(t *testing.T) {
+	for _, tc := range testdataTestCases {
+		original, err := ioutil.ReadFile(filepath.FromSlash(tc.filename) + ".ivg")
+		if err != nil {
+			t.Errorf("%s: ReadFile: %v", tc.filename, err)
+			continue
+		}
+
+		upgraded, err := UpgradeToFileFormatVersion1(original, nil)
+		if err != nil {
+			t.Errorf("%s: Upgrade: %v", tc.filename, err)
+			continue
+		}
+
+		// For most of the testdataTestCases, we just check (above) that
+		// calling UpgradeToFileFormatVersion1 returns a nil error. As a
+		// further basic consistency check, we hard-code the expected results
+		// for upgrading the "action-info.lores" icon.
+		//
+		// These 36 bytes (and its disassembly via the cmd/iconvg-disassemble
+		// tool) is also a file in the test/data directory of the
+		// github.com/google/iconvg repository (the repository that is
+		// generally responsible for "File Format Version 1").
+		if tc.filename == "testdata/action-info.lores" {
+			const want = "" +
+				"\x8A\x49\x56\x47\x03\x0B\x11\x51\x51\xB1\xB1\x35\x81\x59\x33\x59" +
+				"\x81\x81\xA9\x35\x85\x95\x34\x7D\x95\x7D\x7D\x35\x85\x75\x34\x7D" +
+				"\x75\x7D\x6D\x88"
+			if got := string(upgraded); got != want {
+				t.Errorf("%s: Upgrade: got:\n% 02x\nwant:\n% 02x", tc.filename, got, want)
+				continue
+			}
+		}
+	}
+}
