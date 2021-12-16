614eef621c3dc49407f1962f4723e39910b68824
ye.sijun <junnplus@gmail.com>
2021-12-17 00:07:37 +0800

    mmap: use unsafe.Slice rather than maxBytes array

--- mmap/mmap_windows.go
+++ mmap/mmap_windows.go
@@ -108,7 +108,7 @@ func Open(filename string) (*ReaderAt, error) {
 	if err != nil {
 		return nil, err
 	}
-	data := (*[maxBytes]byte)(unsafe.Pointer(ptr))[:size]
+	data := unsafe.Slice((*byte)(unsafe.Pointer(ptr)), size)
 
 	r := &ReaderAt{data: data}
 	if debug {
