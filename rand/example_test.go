6cf2b201936e0673ace5b31d2dffa00dafb3ee58
Axel Wagner <axel.wagner.hh@gmail.com>
2022-02-11 08:34:49 +0100

    rand: make zero LockedSource useful
    By changing LockedSource.src from a pointer to a value its zero value
    becomes usable without panics.
    
    Document this behavior and add an example. Notably, it is not simply a
    Source protected by a sync.Mutex, as Rand also type-asserts on it for
    efficiency.
    
    Fixes golang/go#49342
    
    Change-Id: Iec00766957229b7c67ede37894c9e5b9d6d8f852
    Reviewed-on: https://go-review.googlesource.com/c/exp/+/385094
    Reviewed-by: Rob Pike <r@golang.org>
    Trust: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- rand/example_test.go
+++ rand/example_test.go
@@ -132,3 +132,32 @@ func ExampleShuffle_slicesInUnison() {
 	// B: 2
 	// C: 3
 }
+
+func ExampleLockedSource() {
+	r := rand.New(new(rand.LockedSource))
+	r.Seed(42) // Try changing this number!
+	answers := []string{
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
+	}
+	fmt.Println("Magic 8-Ball says:", answers[r.Intn(len(answers))])
+	// Output: Magic 8-Ball says: Most likely
+}
