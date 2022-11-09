b52ed443b91b3ad2d6f4946bd6a81fa625aed1cf
siarhei <sngasuan@gmail.com>
2022-11-09 20:46:14 +0100

    add bench

--- slices/slices_test.go
+++ slices/slices_test.go
@@ -497,30 +497,37 @@ func TestClone(t *testing.T) {
 }
 
 var compactTests = []struct {
+	name string
 	s    []int
 	want []int
 }{
 	{
+		"nil",
 		nil,
 		nil,
 	},
 	{
+		"one",
 		[]int{1},
 		[]int{1},
 	},
 	{
+		"sorted",
 		[]int{1, 2, 3},
 		[]int{1, 2, 3},
 	},
 	{
+		"1 item",
 		[]int{1, 1, 2},
 		[]int{1, 2},
 	},
 	{
+		"unsorted",
 		[]int{1, 2, 1},
 		[]int{1, 2, 1},
 	},
 	{
+		"many",
 		[]int{1, 2, 2, 3, 3, 4},
 		[]int{1, 2, 3, 4},
 	},
@@ -533,6 +540,17 @@ func TestCompact(t *testing.T) {
 			t.Errorf("Compact(%v) = %v, want %v", test.s, got, test.want)
 		}
 	}
+}
+
+func BenchmarkCompact(b *testing.B) {
+	for _, c := range compactTests {
+		b.Run(c.name, func(b *testing.B) {
+			for k := 0; k < b.N; k++ {
+				_ = Compact(c.s)
+			}
+		})
+	}
+
 }
 
 func TestCompactFunc(t *testing.T) {
