b2135658b1b77bc94efaee4543ec9ad0b88f3d5f
Jonathan Amsterdam <jba@google.com>
2022-12-17 22:18:59 -0500

    slog: update benchmarks to go 1.20rc1
    Nothing is slower; most are a few percent faster.
    
    Change-Id: I3c3fb83ad4202b1aff37ee982cfe671354075e9c
    Reviewed-on: https://go-review.googlesource.com/c/exp/+/458209
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- slog/benchmarks/run_benchmarks.sh
+++ slog/benchmarks/run_benchmarks.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+go=${1:-go}
 
 cd $(dirname $0)
 
-go test -tags nopc -bench . -count 5 > slog.bench
-go test            -bench . -count 5 > slog-pc.bench
+set -x
+
+$go test -tags nopc -bench . -count 5 > slog.bench
+$go test            -bench . -count 5 > slog-pc.bench
