54bba9f4231b280e75ad0d2b0e8fcac09c956e49
Wei Zhang <kweizh@gmail.com>
2023-01-18 00:03:57 +0800

    slog: fix group example panic
    1. group example use *http.Request will panic when r.URL.String()
    2. add Output comment in group example so that go test would check it
    3. add removeTime to group example to make Output deterministic
    
    Change-Id: I51e397654068a3c93375f8c58d9103a28186553c
    Reviewed-on: https://go-review.googlesource.com/c/exp/+/462176
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Run-TryBot: Jonathan Amsterdam <jba@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>

--- slog/example_test.go
+++ slog/example_test.go
@@ -6,20 +6,33 @@ package slog_test
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"golang.org/x/exp/slog"
 )
 
 func ExampleGroup() {
-	var r *http.Request
-	start := time.Now()
+	r, _ := http.NewRequest("GET", "localhost", nil)
 	// ...
+
+	// Remove the time attribute to make Output deterministic.
+	removeTime := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey && len(groups) == 0 {
+			a.Key = ""
+		}
+		return a
+	}
+	logger := slog.New(slog.HandlerOptions{ReplaceAttr: removeTime}.NewTextHandler(os.Stdout))
+	slog.SetDefault(logger)
 
 	slog.Info("finished",
 		slog.Group("req",
 			slog.String("method", r.Method),
 			slog.String("url", r.URL.String())),
 		slog.Int("status", http.StatusOK),
-		slog.Duration("duration", time.Since(start)))
+		slog.Duration("duration", time.Second))
+
+	// Output:
+	// level=INFO msg=finished req.method=GET req.url=localhost status=200 duration=1s
 }
