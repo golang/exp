e0ece0dbea4cee280b7a2a608bac3bb791c5dd2e
Jonathan Amsterdam <jba@google.com>
2025-01-28 09:48:12 -0500

    cmd/{apidiff,gorelease}: use types.Alias when >= 1.23
    This module's go.mod file specifies go1.22.
    These two programs use go/types, which materializes aliases as
    type.Alias when the go version is >= 1.23.
    Force that materialization with toolchains >= 1.23 despite what the
    go.mod says.
    
    Fixes golang/go#70695.
    
    Change-Id: I9401d7f80880fdf59600592baf93bb4c8b7c76ec
    Reviewed-on: https://go-review.googlesource.com/c/exp/+/645015
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- cmd/apidiff/main.go
+++ cmd/apidiff/main.go
@@ -1,3 +1,7 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Command apidiff determines whether two versions of a package are compatible
 package main
 
