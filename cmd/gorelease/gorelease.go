e7979db19e4ef64c2a4b6aa6bfb3a3046606e83b
Shivansh Gupta <98808480+shivanshgupta365@users.noreply.github.com>
2026-07-22 14:09:30 +0530

    cmd/gorelease: replace all self versions during load

--- cmd/gorelease/gorelease.go
+++ cmd/gorelease/gorelease.go
@@ -1082,7 +1082,7 @@ func prepareLoadDir(ctx context.Context, modFile *modfile.File, modPath, modRoot
 	f.AddModuleStmt("gorelease-load-module")
 	f.AddRequire(modPath, version)
 	if !cached {
-		f.AddReplace(modPath, version, modRoot, "")
+		f.AddReplace(modPath, "", modRoot, "")
 	}
 	if modFile != nil {
 		if modFile.Go != nil {
